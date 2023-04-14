35c0c622d667cc910f14106fc77ecff300e63e89
Your Name <you@example.com>
2023-04-14 14:59:16 +0530

    first commit

---
+++ src/test/java/com/cloudConfigueServer/CloudConfigueServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudConfigueServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigueServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
